1cc3d27e5d7a3e2b97ffbdecccaf30420ddd5af8
milan690 <113151517+milan690@users.noreply.github.com>
2024-06-19 11:22:15 -0400

    added comments

--- app/src/main/java/com/example/caloriecalculator/MainActivity.kt
+++ app/src/main/java/com/example/caloriecalculator/MainActivity.kt
@@ -1,3 +1,8 @@
+//File name: calorie counter
+//Author's name: Milan
+//StudentID: 200530596
+//Date: 2024-06-19
+//App description : This is a calorie counter application made as part of mid term exam
 package com.example.caloriecalculator
 
 import android.os.Bundle
